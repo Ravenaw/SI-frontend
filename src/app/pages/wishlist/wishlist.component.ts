9d40d0884bbf9106b204fa75e1be3044ee8cea5e
Vlad F <vlad.faget@outlook.com>
2023-01-02 19:12:41 +0100

    wish you a merry christmas

--- src/app/pages/wishlist/wishlist.component.ts
+++ src/app/pages/wishlist/wishlist.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component,ChangeDetectorRef } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 import { WishlistService } from 'src/app/services/wishlist.service';
+import { ProductDetailsService } from 'src/app/services/product-details.service';
 
 @Component({
   selector: 'app-wishlist',
@@ -7,13 +9,33 @@ import { WishlistService } from 'src/app/services/wishlist.service';
   styleUrls: ['./wishlist.component.css'],
 })
 export class WishlistComponent {
-  constructor(public wishlistService: WishlistService) {}
+  constructor(public wishlistService: WishlistService, public productService: ProductDetailsService,private ref: ChangeDetectorRef) {}
   wishlistData: any = [];
+  temp: any = [];
   ngOnInit() {
-    this.wishlistService.getWishList().subscribe((data) => {
-      this.wishlistData = data.rss.channel.item;
-      console.log(this.wishlistData);
-    });
+    
+
+    const makeRequest = async () => {
+      try {
+        let wishList = await lastValueFrom(this.wishlistService.getWishList());
+        wishList = wishList.rss.channel.item;
+        this.temp = this.temp.concat(wishList);
+        
+        this.temp.forEach(async (element: any) => {
+          let details = await lastValueFrom(this.productService.getProductDetail(element.title._text));
+          details = JSON.parse(JSON.stringify(details.data));
+          details.product.user = element.description._text;
+          this.wishlistData.push(details.product);
+          //console.log(element);
+        });
+        
+        this.ref.detectChanges();
+
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    makeRequest();
     
   }
 }
